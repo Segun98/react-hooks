bc8c944f86d261ef0766f75fa032bdefafe72597
Segun_OS <shegunolanitori@gmail.com>
2021-12-11 23:51:23 +0100

    changes

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -6,10 +6,11 @@ import * as React from 'react'
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(
-    window.localStorage.getItem('name') ?? initialName,
-  )
+  const [name, setName] = React.useState(someExpensiveComputation)
 
+  function someExpensiveComputation() {
+    return window.localStorage.getItem('name') ?? initialName
+  }
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
--- src/exercise/04.js
+++ src/exercise/04.js
@@ -5,8 +5,12 @@ import * as React from 'react'
 
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
-
+  const [squares, setSquares] = React.useState(Array(9).fill(null))
+  const [nextValue, setNextValue] = React.useState(calculateNextValue(squares))
+  const [winner, setWinner] = React.useState(calculateWinner(squares))
+  const [status, setStatus] = React.useState(
+    calculateStatus(winner, squares, nextValue),
+  )
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
   // - winner ('X', 'O', or null)
@@ -21,21 +25,28 @@ function Board() {
     // given square index (like someone clicked a square that's already been
     // clicked), then return early so we don't make any state changes
     //
+    if (winner || squares[square]) return
+
     // 🦉 It's typically a bad idea to mutate or directly change state in React.
     // Doing so can lead to subtle bugs that can easily slip into production.
     //
     // 🐨 make a copy of the squares array
     // 💰 `[...squares]` will do it!)
     //
+    const squaresCopy = [...squares]
     // 🐨 set the value of the square that was selected
     // 💰 `squaresCopy[square] = nextValue`
     //
     // 🐨 set the squares to your copy
+    squaresCopy[square] = nextValue
+
+    setSquares(squaresCopy)
   }
 
   function restart() {
     // 🐨 reset the squares
     // 💰 `Array(9).fill(null)` will do it!
+    setSquares(Array(9).fill(null))
   }
 
   function renderSquare(i) {
